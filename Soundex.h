8733a61f074f82234ec27da06c27eaa6acaf0406
PoorviNK <47118692+PoorviNK@users.noreply.github.com>
2024-09-03 11:13:23 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -6,16 +6,61 @@
 #include <string.h>
 
 char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
+    //c = toupper(c);
+    c = Is_BFPV(toupper(c));
+    c = Is_CGJKQSXZ(toupper(c));
+    c = Is_DT(toupper(c));
+    c = Is_L(toupper(c));
+    c = Is_MN(toupper(c));
+    c = Is_R(toupper(c));
+    c = Is_Vowel(toupper(c));
+    
+    // switch (c) {
+    //     case 'B': case 'F': case 'P': case 'V': return '1';
+    //     case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
+    //     case 'D': case 'T': return '3';
+    //     case 'L': return '4';
+    //     case 'M': case 'N': return '5';
+    //     case 'R': return '6';
+    //     default: return '0'; // For A, E, I, O, U, H, W, Y
+    // }
+    
+    return c;
+}
+char Is_L(char c){
+    if(c == 'L')
+        return '4';
+    else return '0';
+}
+char Is_R(char c){
+    if(c == 'R')
+        return '6';
+    else return '0';
+}
+char Is_MN(char c){
+    if(c == 'M' || c == 'N')
+        return '5';
+    else return '0';
+}
+char Is_Vowel(char c){
+    if(c == 'A' || c == 'E' || c =='I' || c == 'O' || c == 'U')
+        return '0';
+    
+}
+char Is_BFPV(char c){
+    if(c == 'B' || c == 'F' || c =='P' || c == 'V')
+        return '1';
+    else return '0';
+}
+char Is_CGJKQSXZ(char c){
+    if(c == 'C' || c == 'G' || c =='J' || c == 'K' || c == 'Q' c == 'S' || c == 'X' || c == 'Z')
+        return '2';
+    else return '0';
+}
+char Is_DT(char c){
+    if(c == 'D' || c == 'T')
+        return '3';
+    else return '0';
 }
 
 void generateSoundex(const char *name, char *soundex) {
