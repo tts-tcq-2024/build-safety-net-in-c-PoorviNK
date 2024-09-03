0be30f9adb5181145f6c92f9095366d553b8e7a5
PoorviNK <47118692+PoorviNK@users.noreply.github.com>
2024-09-03 18:07:43 +0530

    Update Soundex.Tests.cpp- 4

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -7,3 +7,15 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   generateSoundex("AX", soundex);
   ASSERT_STREQ(soundex,"A200");
 }
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+ //AAA
+  char soundex[5];
+  generateSoundex("Ashcraft", soundex);
+  ASSERT_STREQ(soundex,"A261");
+}
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+ //AAA
+  char soundex[5];
+  generateSoundex("Rupert", soundex);
+  ASSERT_STREQ(soundex,"R163");
+}
