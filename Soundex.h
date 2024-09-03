2944578fcb10c8c535978cd41920d08969aacfde
PoorviNK <47118692+PoorviNK@users.noreply.github.com>
2024-09-03 15:37:47 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,5 +1,6 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
+#define LOGIC (i < len) && (sIndex < 4)
 
 #include "Soundex.h"
 #include <ctype.h>
@@ -65,22 +66,17 @@ char getSoundexCode(char c) {
     
     return c;
 }
+
+
 void generateSoundex(const char *name, char *soundex) {
     int len = strlen(name);
-    if (len == 0) {
-        soundex[0] = '\0';
-        return;
-    }
-
     soundex[0] = toupper(name[0]);
-    char lastCode = '0';
     int sIndex = 1;
 
-    for (int i = 1; i < len && sIndex < 4; i++) {
+    for (int i = 1; LOGIC; i++) {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != lastCode) {
+        if (code != '0' && code != soundex[sIndex - 1]) {
             soundex[sIndex++] = code;
-            lastCode = code;
         }
     }
 
@@ -92,24 +88,3 @@ void generateSoundex(const char *name, char *soundex) {
 }
 
 #endif // SOUNDEX_H
-
-// void generateSoundex(const char *name, char *soundex) {
-//     int len = strlen(name);
-//     soundex[0] = toupper(name[0]);
-//     int sIndex = 1;
-
-//     for (int i = 1; (i < len) && (sIndex < 4); i++) {
-//         char code = getSoundexCode(name[i]);
-//         if (code != '0' && code != soundex[sIndex - 1]) {
-//             soundex[sIndex++] = code;
-//         }
-//     }
-
-//     while (sIndex < 4) {
-//         soundex[sIndex++] = '0';
-//     }
-
-//     soundex[4] = '\0';
-// }
-
-// #endif // SOUNDEX_H
